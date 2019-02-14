ae3491ebcb83a2113c150cdfa4c5989511359098
Kevin M. Cotter <kmc4518@gmail.com>
2019-02-14 09:49:39 -0500

    level 3 functionality

---
+++ js/level3.js
@@ -0,0 +1,24 @@
+var answers = ["Chickadee", "Squirtle", "Puzzle"];
+var inputs = [];
+
+function checkInputs() {
+    var a1 = document.getElementById("ans1").value;
+    var a2 = document.getElementById("ans2").value;
+    var a3 = document.getElementById("ans3").value;
+    inputs.push( a1, a2, a3)
+
+    if (JSON.stringify(inputs) === JSON.stringify(answers))
+    {
+        alert("you win ");
+        Cookies.set('level', '4', { expires: 100 });
+        window.location = "index.html";
+    }
+    else
+    {
+        alert("you lose!")
+        Cookies.set('level', '0', { expires: 100 });
+        window.location = "index.html";
+    }
+
+    inputs = [];
+}
